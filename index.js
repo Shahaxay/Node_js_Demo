986f838b9c8435a4b284dac0610df4de15b2ca36
Akshayshah47 <removeit61@gmail.com>
2023-06-10 08:54:41 +0530

    fat arrow function and student obj

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+
+//fat arrow function
+const product=(a,b)=>a*b;
+
+console.log(product(5,6));
+
+//student object
+const student={
+
+name:"AK",
+phone:"123"
+
+}
+
+//displaying name of the student object
+
+console.log(student.name)
