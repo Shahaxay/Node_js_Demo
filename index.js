9e0fc69ad09f2ae476ed1b8982e85aed7a793e8e
Akshayshah47 <removeit61@gmail.com>
2023-06-10 09:08:09 +0530

    map function in array

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ phone:"123"
 //displaying name of the student object
 
 console.log(student.name)
+
+//map
+const ar=['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+const modified_ar=ar.map((ele)=>{
+    if(ele===' '){
+        return 'empty string';
+    }
+    return ele;
+})
+console.log(modified_ar);
